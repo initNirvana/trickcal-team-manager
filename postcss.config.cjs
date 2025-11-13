ec5c1c86e396bf6432df025da9a8553218cbf9d1
unknown <kimqqyun@gmail.com>
2025-11-13 23:33:26 +0900

    chore : initial package & build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), flowbiteReact()],
   test: {
     globals: true,
     environment: "jsdom",
