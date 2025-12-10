bdd70ceeb1f013166362e1ec63d550878050b341
khkim <khkim@linkprice.com>
2025-12-10 15:18:44 +0900

    chore: tsconfig 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vitest/config';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
