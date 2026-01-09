9be588da57dc47393eb5ba19f5556a542aa56ba1
khkim <khkim@linkprice.com>
2026-01-09 16:34:00 +0900

    feat : VitePWA 지원

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,52 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import SsgoiAutoKey from '@ssgoi/react/unplugin/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { icons } from './src/AppImages/icons.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss(), tsconfigPaths(), SsgoiAutoKey()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    SsgoiAutoKey(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: '트릭컬 매니저',
+        short_name: '트릭컬 매니저',
+        description: '트릭컬 도구 사이트',
+        lang: 'ko',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        display_override: ['standalone', 'window-controls-overlay'],
+        icons: icons.map((icon) => {
+          const isMaskable = icon.src.includes('android-launchericon');
+          return {
+            src: `/AppImages/${icon.src}`,
+            type: 'image/png',
+            sizes: icon.sizes,
+            purpose: isMaskable ? 'any maskable' : 'any',
+          };
+        }),
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff2}'],
+      },
+    }),
+  ],
   publicDir: 'public',
   test: {
     globals: true,
